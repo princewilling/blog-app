eeab7e94d9030e2dbc8ca96d666ce1be99c9825f
princewilling <princewillingoo@gmail.com>
2023-05-19 09:20:55 +0100

    add image field to blog.post model

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,11 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    image = models.ImageField(
+        upload_to='posts/%Y/%m/%d/', 
+        blank=True,
+        null=True
+    )
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
