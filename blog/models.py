a536ad2bbd97d218723013a28919030519bb9ac9
princewilling <princewillingoo@gmail.com>
2023-05-20 07:48:50 +0100

    blog.post model manager

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+        return super().get_queryset()\
+            .filter(status=Post.Status.PUBLISHED)
 
 class Post(models.Model):
     
@@ -29,6 +35,8 @@ class Post(models.Model):
         choices=Status.choices, 
         default=Status.DRAFT
     )
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
     
     class Meta:
         ordering = ['-publish']
