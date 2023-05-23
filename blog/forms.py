5ed79c0abb56fe2016e3185fbba8c8e376c89525
princewilling <princewillingoo@gmail.com>
2023-05-23 15:27:50 +0100

    functionality to recommend posts by email:creating email form

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class EmialPostForm(forms.Form):
+    name = forms.CharField(max_length=25)
+    email = forms.EmailField()
+    to = forms.EmailField()
+    comments = forms.CharField(required=False, 
+                               widget=forms.Textarea)
