4cf8b7d121777c18cf43e958a1623dee115757e2
princewilling <princewillingoo@gmail.com>
2023-05-24 08:46:21 +0100

    adding comments to the post detail view

--- blog/views.py
+++ blog/views.py
@@ -47,11 +47,14 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
     )
-    
+    # List of active comments for the this post
+    comments = post.comments.filter(active=True)
+    # From for users to comment
+    form = CommentForm()
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {'post': post, 'comments': comments, 'form': form}
     )
     
 def post_share(request, post_id):
