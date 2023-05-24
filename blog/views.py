bb21d2baaac9ba5058ecbbaf6117e4feb81ce261
princewilling <princewillingoo@gmail.com>
2023-05-24 08:16:54 +0100

    handling comment modelform in views

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmialPostForm
+from .forms import EmialPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 class PostListView(ListView):
     """Alternative post list view
@@ -77,3 +77,20 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create c comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post':post, 
+                                                      'form': form, 
+                                                      'comment': comment})
