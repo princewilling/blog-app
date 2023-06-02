400ad9ff570638eefd3cb2b08ec4f10716e9954c
princewilling <princewillingoo@gmail.com>
2023-06-02 21:52:58 +0100

    comment

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView
 from .forms import EmialPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+
 
 class PostListView(ListView):
     """Alternative post list view
@@ -18,8 +20,12 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html'
     
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -34,7 +40,7 @@ def post_list(request):
     return render(
         request,
         'blog/post/list.html',
-        {'posts': posts}
+        {'posts': posts, 'tag':tag}
     )
     
 
