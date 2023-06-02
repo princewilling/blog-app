c6573cd6d35de48dd5d07cccc5a1a832f3bb9a82
princewilling <princewillingoo@gmail.com>
2023-06-02 23:12:52 +0100

    Retrieving posts by similarity based on tags

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmialPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from django.db.models import Count
 from taggit.models import Tag
 
 
@@ -57,10 +58,22 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # From for users to comment
     form = CommentForm()
+    
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+        .order_by('-same_tags', 'publish')[:5]
+    
+    
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post, 'comments': comments, 'form': form}
+        {'post': post, 
+         'comments': comments, 
+         'form': form,
+         'similar_posts': similar_posts}
     )
     
 def post_share(request, post_id):
